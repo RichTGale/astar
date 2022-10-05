b2a77332976aadba82e1fb56a59b86efcebf6abc
richtgale <richtgale@gmail.com>
2022-10-06 09:38:58 +1100

    Dir-structure corrections for cmake

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -H. -BDebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
-ln -s Debug/compile_commands.json .
+cmake -H. -Bdebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
+ln -s debug/compile_commands.json .
