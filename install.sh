76fba2463b77c99df335c7fd1bc226ba5fc9eb72
richtgale <richtgale@gmail.com>
2022-10-06 20:27:07 +1100

    refactored cmake build

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cmake -H. -Bdebug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
-ln -s debug/compile_commands.json .
+cmake -H. -B./ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 
-make --directory=./debug
+make --directory=./
